aa5ee8e9ed27e102274bf6de33401d3c55f50f69
ミドリ <midori01@users.noreply.github.com>
2023-06-16 09:02:10 +0800

    Create install.sh

---
+++ simple-obfs/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt update && apt install --no-install-recommends build-essential autoconf libtool libssl-dev libpcre3-dev libev-dev asciidoc xmlto automake -y
+git clone https://github.com/shadowsocks/simple-obfs.git
+cd simple-obfs
+git submodule update --init --recursive
+./autogen.sh
+./configure && make
+make install
+cd ..
+rm -r simple-obfs
