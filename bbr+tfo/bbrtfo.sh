2a4116d32f601cd3a6cbf8dea815e2896e3a3952
ミドリ <midori01@users.noreply.github.com>
2023-06-16 10:05:04 +0800

    Create bbrtfo.sh

---
+++ bbr+tfo/bbrtfo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+  echo "请切换到 root 用户后再运行脚本"
+  exit 1
+fi
+
+echo net.core.default_qdisc=fq >> /etc/sysctl.conf
+echo net.ipv4.tcp_congestion_control=bbr >> /etc/sysctl.conf
+echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf
+echo 3 > /proc/sys/net/ipv4/tcp_fastopen
+echo net.ipv4.tcp_fastopen=3 >> /etc/sysctl.conf
+
+sysctl -p
+
+echo "BBR & TCP Fast Open 已开启"
