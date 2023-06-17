7bd0b1075b75b4ef440a7658e2018169e652bbf8
ミドリ <midori01@users.noreply.github.com>
2023-06-17 22:37:40 +0800

    Update and rename bbrtfo.sh to bbr.sh

--- network/bbr.sh
+++ network/bbr.sh
@@ -7,10 +7,7 @@ fi
 
 echo net.core.default_qdisc=fq >> /etc/sysctl.conf
 echo net.ipv4.tcp_congestion_control=bbr >> /etc/sysctl.conf
-echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf
-echo 3 > /proc/sys/net/ipv4/tcp_fastopen
-echo net.ipv4.tcp_fastopen=3 >> /etc/sysctl.conf
 
 sysctl -p
 
-echo "BBR & TCP Fast Open & IPv4 Forward 已开启"
+echo "BBR 已开启"
