3571bf1c1f994603d714d9c7a9ed0eaf3b8d56be
ミドリ <midori01@users.noreply.github.com>
2023-06-17 22:07:08 +0800

    Create ipv6.sh

---
+++ oracle/ipv6.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+  echo "请切换到 root 用户后再运行脚本"
+  exit 1
+fi
+
+if ! command -v ifup &> /dev/null; then
+  echo "ifupdown 未安装，请安装后再运行脚本"
+  exit 1
+fi
+
+netdevice=$(ip route get 1.1.1.1 | awk -F"dev " '{print $2}' | awk '{print $1; exit}')
+echo "iface ${netdevice} inet6 dhcp" >> /etc/network/interfaces
+ifdown ${netdevice} && ifup ${netdevice}
+
+echo "IPv6 地址已添加"
+echo "$(curl -s ip.sb -6)"
