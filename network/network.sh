e6c8caf35a57569a9be460239a79319846aafbbf
ミドリ <midori01@users.noreply.github.com>
2023-06-18 21:20:43 +0800

    Update network.sh

--- network/network.sh
+++ network/network.sh
@@ -70,6 +70,62 @@ sysctl -p && sysctl --system
 echo "内核参数已调整"
 }
 
+ulimit() {
+echo "1000000" > /proc/sys/fs/file-max
+sed -i '/fs.file-max/d' /etc/sysctl.conf
+cat >> '/etc/sysctl.conf' << EOF
+fs.file-max=1000000
+EOF
+ulimit -SHn 1000000 && ulimit -c unlimited
+echo "root     soft   nofile    1000000
+root     hard   nofile    1000000
+root     soft   nproc     1000000
+root     hard   nproc     1000000
+root     soft   core      1000000
+root     hard   core      1000000
+root     hard   memlock   unlimited
+root     soft   memlock   unlimited
+
+*     soft   nofile    1000000
+*     hard   nofile    1000000
+*     soft   nproc     1000000
+*     hard   nproc     1000000
+*     soft   core      1000000
+*     hard   core      1000000
+*     hard   memlock   unlimited
+*     soft   memlock   unlimited
+">/etc/security/limits.conf
+if grep -q "ulimit" /etc/profile; then
+  :
+else
+  sed -i '/ulimit -SHn/d' /etc/profile
+  echo "ulimit -SHn 1000000" >>/etc/profile
+fi
+if grep -q "pam_limits.so" /etc/pam.d/common-session; then
+  :
+else
+  sed -i '/required pam_limits.so/d' /etc/pam.d/common-session
+  echo "session required pam_limits.so" >>/etc/pam.d/common-session
+fi
+sed -i '/DefaultTimeoutStartSec/d' /etc/systemd/system.conf
+sed -i '/DefaultTimeoutStopSec/d' /etc/systemd/system.conf
+sed -i '/DefaultRestartSec/d' /etc/systemd/system.conf
+sed -i '/DefaultLimitCORE/d' /etc/systemd/system.conf
+sed -i '/DefaultLimitNOFILE/d' /etc/systemd/system.conf
+sed -i '/DefaultLimitNPROC/d' /etc/systemd/system.conf
+cat >>'/etc/systemd/system.conf' <<EOF
+[Manager]
+#DefaultTimeoutStartSec=90s
+DefaultTimeoutStopSec=30s
+#DefaultRestartSec=100ms
+DefaultLimitCORE=infinity
+DefaultLimitNOFILE=65535
+DefaultLimitNPROC=65535
+EOF
+systemctl daemon-reload
+echo "ulimit 已调整"
+}
+
 if [[ $1 == "bbr" ]]; then
   bbr
   exit 0
@@ -92,5 +148,10 @@ fi
 
 if [[ $1 == "network" ]]; then
   network
+  exit 0
+fi
+
+if [[ $1 == "ulimit" ]]; then
+  ulimit
   exit 0
 fi
