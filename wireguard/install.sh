c95739f9f02625fc0617146e7689aa8f35e69d33
ミドリ <midori01@users.noreply.github.com>
2023-06-18 23:06:43 +0800

    Update install.sh

--- wireguard/install.sh
+++ wireguard/install.sh
@@ -18,8 +18,28 @@ uninstall() {
   echo "WireGuard 已卸载"
 }
 
+peer() {
+  read -r -p "请输入备注: " note
+  read -r -p "请输入 Peer 公钥: " peer_public_key
+  read -r -p "请输入 Self IP: " peer_self_ipv4
+  last_octet=$(echo "${peer_self_ipv4}" | awk -F. '{print $4}')
+  peer_self_ipv6="fd10::${last_octet}"
+  echo "" >> /etc/wireguard/wg0.conf
+  echo "# ${note}" >> /etc/wireguard/wg0.conf
+  echo "[Peer]" >> /etc/wireguard/wg0.conf
+  echo "PublicKey = ${peer_public_key}" >> /etc/wireguard/wg0.conf
+  echo "AllowedIPs = ${peer_self_ipv4}/32, ${peer_self_ipv6}/128" >> /etc/wireguard/wg0.conf
+  wg syncconf wg0 <(wg-quick strip wg0)
+  wg show wg0
+}
+
 if [[ $1 == "uninstall" ]]; then
   uninstall
+  exit 0
+fi
+
+if [[ $1 == "peer" ]]; then
+  peer
   exit 0
 fi
 
