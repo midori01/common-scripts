c9b4e2a5affcb9326edea74f05754bbf915d2276
ミドリ <midori01@users.noreply.github.com>
2023-06-19 20:47:21 +0900

    Update install.sh

--- shadow-tls/install.sh
+++ shadow-tls/install.sh
@@ -8,9 +8,7 @@ if ! command -v wget &> /dev/null; then
   echo "wget 未安装，请安装后再运行脚本"
   exit 1
 fi
-
 shadowtls_version=$(curl -m 10 -sL "https://api.github.com/repos/ihciah/shadow-tls/releases/latest" | awk -F'"' '/tag_name/{print $4}')
-
 if [[ "$(uname -m)" == "x86_64" ]]; then
   shadowtls_type="x86_64"
 elif [[ "$(uname -m)" == "aarch64" ]]; then
@@ -19,7 +17,6 @@ else
   echo "$(uname -m) 架构不支持"
   exit 1
 fi
-
 uninstall() {
   systemctl stop shadow-tls.service
   systemctl disable shadow-tls.service
@@ -28,7 +25,6 @@ uninstall() {
   rm -f /usr/local/bin/shadow-tls
   echo "Shadow-TLS 已卸载"
 }
-
 update() {
   rm -f /usr/local/bin/shadow-tls
   wget -O /usr/local/bin/shadow-tls "https://github.com/ihciah/shadow-tls/releases/download/${shadowtls_version}/shadow-tls-${shadowtls_type}-unknown-linux-musl"
@@ -36,7 +32,6 @@ update() {
   systemctl restart shadow-tls.service
   echo "Shadow-TLS 已更新"
 }
-
 if [[ $1 == "uninstall" ]]; then
   uninstall
   exit 0
@@ -45,20 +40,15 @@ if [[ $1 == "update" ]]; then
   update
   exit 0
 fi
-
 read -r -p "请输入 Shadow-TLS 监听端口 (留空默认 443): " shadowtls_port
 shadowtls_port=${shadowtls_port:-443}
-
 read -r -p "请输入 Shadow-TLS 密码 (留空随机生成): " shadowtls_password
 if [[ -z "$shadowtls_password" ]]; then
   shadowtls_password=$(openssl rand -base64 32)
 fi
-
 read -r -p "请输入 Shadow-TLS SNI (留空默认 www.iq.com): " sni_domain
 sni_domain=${sni_domain:-www.iq.com}
-
 read -r -p "请输入本机其他 TCP 代理协议端口: " server_port
-
 cat <<EOF
 请确认以下配置信息：
 Shadow-TLS 端口：${shadowtls_port}
@@ -71,10 +61,8 @@ case "$confirm" in
   [yY]) ;;
   *) echo "已取消安装"; exit 0;;
 esac
-
 wget -O /usr/local/bin/shadow-tls "https://github.com/ihciah/shadow-tls/releases/download/${shadowtls_version}/shadow-tls-${shadowtls_type}-unknown-linux-musl"
 chmod +x /usr/local/bin/shadow-tls
-
 cat > /etc/systemd/system/shadow-tls.service <<EOF
 [Unit]
 Description=shadow-tls service
@@ -93,7 +81,6 @@ SyslogIdentifier=shadow-tls
 [Install]
 WantedBy=multi-user.target
 EOF
-
 cat > /etc/shadow-tls.json <<EOF
 {
   "disable_nodelay": false,
@@ -116,11 +103,9 @@ cat > /etc/shadow-tls.json <<EOF
   }
 }
 EOF
-
 systemctl daemon-reload
 systemctl start shadow-tls.service
 systemctl enable shadow-tls.service
-
 echo "Shadow-TLS 安装成功"
 echo "客户端连接信息: "
 echo "连接端口: ${shadowtls_port}"
