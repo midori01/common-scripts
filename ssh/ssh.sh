0dd347100e8b6ac9bb2fd023ad1dddaef97570bc
ミドリ <midori01@users.noreply.github.com>
2023-06-19 20:53:00 +0900

    Update ssh.sh

--- ssh/ssh.sh
+++ ssh/ssh.sh
@@ -4,7 +4,6 @@ if [[ $EUID -ne 0 ]]; then
   echo "请切换到 root 用户后再运行脚本"
   exit 1
 fi
-
 sshkey() {
   echo "请粘贴公钥内容："
   read ssh_public_key
@@ -16,25 +15,21 @@ sshkey() {
   service sshd restart
   echo "SSH 公钥已添加"
 }
-
 enablepwd() {
   sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
   service sshd restart
   echo "密码登录已开启"
 }
-
 disablepwd() {
   sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/g' /etc/ssh/sshd_config
   service sshd restart
   echo "密码登录已关闭"
 }
-
 rootlogin() {
   sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
   service sshd restart
   echo "root 登录已开启"
 }
-
 port() {
   read -p "请输入新 SSH 端口：" ssh_port
   read -p "您输入的新 SSH 端口为：${ssh_port}，是否确认修改？[Y/n]" confirm
@@ -46,27 +41,22 @@ port() {
   service sshd restart
   echo "SSH 端口已修改"
 }
-
 if [[ $1 == "sshkey" ]]; then
   sshkey
   exit 0
 fi
-
 if [[ $1 == "enablepwd" ]]; then
   enablepwd
   exit 0
 fi
-
 if [[ $1 == "disablepwd" ]]; then
   disablepwd
   exit 0
 fi
-
 if [[ $1 == "rootlogin" ]]; then
   rootlogin
   exit 0
 fi
-
 if [[ $1 == "port" ]]; then
   port
   exit 0
