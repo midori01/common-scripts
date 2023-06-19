2aac70c210754cb6c1b246d28c48b63de8be2829
ミドリ <midori01@users.noreply.github.com>
2023-06-19 20:57:39 +0900

    Update install.sh

--- vmess/install.sh
+++ vmess/install.sh
@@ -4,40 +4,31 @@ if [[ $EUID -ne 0 ]]; then
   echo "请切换到 root 用户后再运行脚本"
   exit 1
 fi
-
-apt install -y uuid-runtime
-
 uninstall() {
   bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ remove --purge
   echo "VMess 已卸载"
 }
-
 update() {
   bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta -u root
   echo "VMess 已更新"
 }
-
 if [[ $1 == "uninstall" ]]; then
   uninstall
   exit 0
 fi
-
 if [[ $1 == "update" ]]; then
   update
   exit 0
 fi
-
+apt install -y uuid-runtime
 read -r -p "请输入 VMess 端口 (留空默认 1024): " vmess_port
 vmess_port=${vmess_port:-1024}
-
 read -r -p "请输入 VMess UUID (留空随机生成): " vmess_uuid
 if [[ -z "$vmess_uuid" ]]; then
   vmess_uuid=$(uuidgen)
 fi
-
 read -r -p "请输入 WebSocket 路径 (留空默认 /): " ws_path
 ws_path=${ws_path:-/}
-
 cat <<EOF
 请确认以下配置信息：
 端口：${vmess_port}
@@ -50,9 +41,7 @@ case "$confirm" in
   [yY]) ;;
   *) echo "已取消安装"; exit 0;;
 esac
-
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta -u root
-
 cat > /usr/local/etc/xray/config.json <<EOF
 {
     "log": {
@@ -92,9 +81,7 @@ cat > /usr/local/etc/xray/config.json <<EOF
     ]
 }
 EOF
-
 systemctl restart xray.service
-
 echo "VMess 安装成功"
 echo "客户端连接信息: "
 echo "端口: ${vmess_port}"
