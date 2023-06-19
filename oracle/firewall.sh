5e27a833d06d94d1c9c049b4084e08e01f7158c7
ミドリ <midori01@users.noreply.github.com>
2023-06-19 20:44:03 +0900

    Update firewall.sh

--- oracle/firewall.sh
+++ oracle/firewall.sh
@@ -4,12 +4,10 @@ if [[ $EUID -ne 0 ]]; then
   echo "请切换到 root 用户后再运行脚本"
   exit 1
 fi
-
 iptables -P INPUT ACCEPT
 iptables -P FORWARD ACCEPT
 iptables -P OUTPUT ACCEPT
 iptables -F
 apt purge netfilter-persistent -y
 rm -rf /etc/iptables
-
 echo "已删除系统自带防火墙"
