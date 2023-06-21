17923b9b5887d53bec9c9fe46e63ad64647308ac
ミドリ <midori01@users.noreply.github.com>
2023-06-21 17:41:56 +0800

    Update install.sh

--- caddy/install.sh
+++ caddy/install.sh
@@ -30,8 +30,8 @@ if [[ $1 == "reverse" ]]; then
   exit 0
 fi
 apt install -y debian-keyring debian-archive-keyring apt-transport-https
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
 apt update
 apt install -y caddy
 > /etc/caddy/Caddyfile
