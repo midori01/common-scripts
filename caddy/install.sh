c6946f1d8bac201bc49ef75fd10cc3909add729d
ミドリ <midori01@users.noreply.github.com>
2023-06-21 16:58:09 +0800

    Update and rename hysteria/caddy/install.sh to caddy/install.sh

--- caddy/install.sh
+++ caddy/install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+  echo "请切换到 root 用户后再运行脚本"
+  exit 1
+fi
 apt install -y debian-keyring debian-archive-keyring apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 apt update
 apt install -y caddy
+echo "Caddy 安装成功"
