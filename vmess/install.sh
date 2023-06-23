8abc0a10fb736cbfb3287d5099f5a7f3a65126ec
ミドリ <midori01@users.noreply.github.com>
2023-06-23 10:52:43 +0800

    Update install.sh

--- vmess/install.sh
+++ vmess/install.sh
@@ -191,12 +191,12 @@ cat > /usr/local/etc/xray/config.json <<EOF
                 "network": "tcp",
                 "security": "tls",
                 "tlsSettings": {
-                  "certificates": [
-                    {
-                      "certificateFile": "${cer_path}",
-                      "keyFile": "${key_path}"
-                    }
-                  ]
+                    "certificates": [
+                        {
+                            "certificateFile": "${cer_path}",
+                            "keyFile": "${key_path}"
+                        }
+                    ]
                 }
             }
         }
@@ -272,12 +272,12 @@ cat > /usr/local/etc/xray/config.json <<EOF
                     "path": "${ws_path}"
                 },
                 "tlsSettings": {
-                  "certificates": [
-                    {
-                      "certificateFile": "${cer_path}",
-                      "keyFile": "${key_path}"
-                    }
-                  ]
+                    "certificates": [
+                        {
+                            "certificateFile": "${cer_path}",
+                            "keyFile": "${key_path}"
+                        }
+                    ]
                 }
             }
         }
@@ -299,6 +299,155 @@ echo "端口: ${vmess_port}"
 echo "UUID: ${vmess_uuid}"
 echo "传输: WebSocket"
 echo "路径: ${ws_path}"
+echo "TLS: Enabled"
+}
+grpc() {
+read -r -p "请输入 VMess 端口 (留空默认 1024): " vmess_port
+vmess_port=${vmess_port:-1024}
+read -r -p "请输入 VMess UUID (留空随机生成): " vmess_uuid
+if [[ -z "$vmess_uuid" ]]; then
+  vmess_uuid=$(uuidgen)
+fi
+read -r -p "请输入服务名称（域名）: " service_name
+cat <<EOF
+请确认以下配置信息：
+端口：${vmess_port}
+UUID：${vmess_uuid}
+传输：gRPC
+服务名称：${service_name}
+EOF
+read -r -p "确认无误？(Y/N)" confirm
+case "$confirm" in
+  [yY]) ;;
+  *) echo "已取消安装"; exit 0;;
+esac
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta -u root
+cat > /usr/local/etc/xray/config.json <<EOF
+{
+    "log": {
+        "loglevel": "warning"
+    },
+    "routing": {
+        "domainStrategy": "AsIs"
+    },
+    "inbounds": [
+        {
+            "listen": "0.0.0.0",
+            "port": ${vmess_port},
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "${vmess_uuid}"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "grpc",
+                "security": "none",
+                "grpcSettings": {
+                    "serviceName": "${service_name}"
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom",
+            "settings": {
+                "domainStrategy": "UseIPv4"
+            }
+        }
+    ]
+}
+EOF
+systemctl restart xray.service
+echo "VMess 安装成功"
+echo "客户端连接信息: "
+echo "端口: ${vmess_port}"
+echo "UUID: ${vmess_uuid}"
+echo "传输: gRPC"
+echo "服务名称: ${service_name}"
+}
+grpc-tls() {
+read -r -p "请输入 VMess 端口 (留空默认 1024): " vmess_port
+vmess_port=${vmess_port:-1024}
+read -r -p "请输入 VMess UUID (留空随机生成): " vmess_uuid
+if [[ -z "$vmess_uuid" ]]; then
+  vmess_uuid=$(uuidgen)
+fi
+read -r -p "请输入服务名称（域名）: " service_name
+read -r -p "请输入证书文件路径: " cer_path
+read -r -p "请输入私钥文件路径: " key_path
+cat <<EOF
+请确认以下配置信息：
+端口：${vmess_port}
+UUID：${vmess_uuid}
+传输：gRPC
+服务名称：${service_name}
+证书路径：${cer_path}
+私钥路径：${key_path}
+EOF
+read -r -p "确认无误？(Y/N)" confirm
+case "$confirm" in
+  [yY]) ;;
+  *) echo "已取消安装"; exit 0;;
+esac
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta -u root
+cat > /usr/local/etc/xray/config.json <<EOF
+{
+    "log": {
+        "loglevel": "warning"
+    },
+    "routing": {
+        "domainStrategy": "AsIs"
+    },
+    "inbounds": [
+        {
+            "listen": "0.0.0.0",
+            "port": ${vmess_port},
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "${vmess_uuid}"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "grpc",
+                "security": "tls",
+                "grpcSettings": {
+                    "serviceName": "${service_name}"
+                },
+                "tlsSettings": {
+                    "certificates": [
+                        {
+                            "certificateFile": "${cer_path}",
+                            "keyFile": "${key_path}"
+                        }
+                    ]
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom",
+            "settings": {
+                "domainStrategy": "UseIPv4"
+            }
+        }
+    ]
+}
+EOF
+systemctl restart xray.service
+echo "VMess 安装成功"
+echo "客户端连接信息: "
+echo "端口: ${vmess_port}"
+echo "UUID: ${vmess_uuid}"
+echo "传输: gRPC"
+echo "服务名称: ${service_name}"
 echo "TLS: Enabled"
 }
 if [[ $1 == "uninstall" ]]; then
@@ -323,5 +472,13 @@ if [[ $1 == "tls" ]]; then
 fi
 if [[ $1 == "wss" ]]; then
   wss
+  exit 0
+fi
+if [[ $1 == "grpc" ]]; then
+  grpc
+  exit 0
+fi
+if [[ $1 == "grpc-tls" ]]; then
+  grpc-tls
   exit 0
 fi
