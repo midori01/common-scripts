9e3d107f0922a46a9d6eba8f6a34816e3bba93f0
ミドリ <midori01@users.noreply.github.com>
2023-06-26 11:09:53 +0800

    Update and rename install.sh to install.sh

--- xray/install.sh
+++ xray/install.sh
@@ -7,11 +7,11 @@ fi
 apt install -y uuid-runtime
 uninstall() {
   bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ remove --purge
-  echo "VMess 已卸载"
+  echo "Xray-core 已卸载"
 }
 update() {
   bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta -u root
-  echo "VMess 已更新"
+  echo "Xray-core 已更新"
 }
 tcp() {
 read -r -p "请输入 VMess 端口 (留空默认 1024): " vmess_port
@@ -537,6 +537,243 @@ echo "QUIC 加密: none"
 echo "QUIC 伪装: ${obfs}"
 echo "TLS: Enabled"
 }
+vless() {
+read -r -p "请输入端口 (留空默认 1024): " vless_port
+vless_port=${vless_port:-1024}
+read -r -p "请输入 UUID (留空随机生成): " vless_uuid
+if [[ -z "$vless_uuid" ]]; then
+  vless_uuid=$(uuidgen)
+fi
+read -r -p "请输入 SNI 域名 (留空默认 www.iq.com): " sni_domain
+sni_domain=${sni_domain:-www.iq.com}
+cat <<EOF
+请确认以下配置信息：
+端口：${vless_port}
+UUID：${vless_uuid}
+SNI：${sni_domain}
+EOF
+read -r -p "确认无误？(Y/N)" confirm
+case "$confirm" in
+  [yY]) ;;
+  *) echo "已取消安装"; exit 0;;
+esac
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta -u root
+output=$(/usr/local/bin/xray x25519)
+privatekey=$(echo "$output" | awk '/Private key:/ {print $NF}')
+publickey=$(echo "$output" | awk '/Public key:/ {print $NF}')
+shortid=$(openssl rand -hex 8)
+cat > /usr/local/etc/xray/config.json <<EOF
+{
+    "log": {
+        "loglevel": "warning"
+    },
+    "routing": {
+        "domainStrategy": "AsIs"
+    },
+    "inbounds": [
+        {
+            "listen": "0.0.0.0",
+            "port": ${vless_port},
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "${vless_uuid}",
+                        "flow": "xtls-rprx-vision"
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "security": "reality",
+                "realitySettings": {
+                    "show": false,
+                    "dest": "${sni_domain}:443",
+                    "xver": 0,
+                    "serverNames": ["${sni_domain}"],
+                    "privateKey": "${privatekey}",
+                    "shortIds": ["${shortid}"]
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom",
+            "settings": {
+                "domainStrategy": "UseIPv4"
+            }
+        }
+    ]
+}
+EOF
+systemctl restart xray.service
+echo "VLESS REALITY 安装成功"
+echo "客户端连接信息: "
+echo "端口: ${vless_port}"
+echo "UUID: ${vless_uuid}"
+echo "传输: TCP"
+echo "Flow: xtls-rprx-vision"
+echo "Public Key: ${publickey}"
+echo "Short ID: ${shortid}"
+echo "SNI: ${sni_domain}"
+}
+trojan-tcp() {
+read -r -p "请输入端口 (留空默认 1024): " trojan_port
+trojan_port=${trojan_port:-1024}
+read -r -p "请输入密码 (留空随机生成): " trojan_password
+if [[ -z "$trojan_password" ]]; then
+  trojan_password=$(openssl rand -base64 16)
+fi
+read -r -p "请输入证书文件路径: " cer_path
+read -r -p "请输入私钥文件路径: " key_path
+cat <<EOF
+请确认以下配置信息：
+端口：${trojan_port}
+密码：${trojan_password}
+传输：TCP
+证书路径：${cer_path}
+私钥路径：${key_path}
+EOF
+read -r -p "确认无误？(Y/N)" confirm
+case "$confirm" in
+  [yY]) ;;
+  *) echo "已取消安装"; exit 0;;
+esac
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta -u root
+cat > /usr/local/etc/xray/config.json <<EOF
+{
+    "log": {
+        "loglevel": "warning"
+    },
+    "routing": {
+        "domainStrategy": "AsIs"
+    },
+    "inbounds": [
+        {
+            "listen": "0.0.0.0",
+            "port": ${trojan_port},
+            "protocol": "trojan",
+            "settings": {
+                "clients": [
+                    {
+                        "password": "${trojan_password}"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "security": "tls",
+                "tlsSettings": {
+                    "certificates": [
+                        {
+                            "certificateFile": "${cer_path}",
+                            "keyFile": "${key_path}"
+                        }
+                    ]
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom",
+            "settings": {
+                "domainStrategy": "UseIPv4"
+            }
+        }
+    ]
+}
+EOF
+systemctl restart xray.service
+echo "Trojan 安装成功"
+echo "客户端连接信息: "
+echo "端口: ${trojan_port}"
+echo "密码: ${trojan_password}"
+echo "传输: TCP"
+}
+trojan-ws() {
+read -r -p "请输入端口 (留空默认 1024): " trojan_port
+trojan_port=${trojan_port:-1024}
+read -r -p "请输入密码 (留空随机生成): " trojan_password
+if [[ -z "$trojan_password" ]]; then
+  trojan_password=$(openssl rand -base64 16)
+fi
+read -r -p "请输入 WebSocket 路径 (留空默认 /): " ws_path
+ws_path=${ws_path:-/}
+read -r -p "请输入证书文件路径: " cer_path
+read -r -p "请输入私钥文件路径: " key_path
+cat <<EOF
+请确认以下配置信息：
+端口：${trojan_port}
+密码：${trojan_password}
+传输：WebSocket
+路径：${ws_path}
+证书路径：${cer_path}
+私钥路径：${key_path}
+EOF
+read -r -p "确认无误？(Y/N)" confirm
+case "$confirm" in
+  [yY]) ;;
+  *) echo "已取消安装"; exit 0;;
+esac
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta -u root
+cat > /usr/local/etc/xray/config.json <<EOF
+{
+    "log": {
+        "loglevel": "warning"
+    },
+    "routing": {
+        "domainStrategy": "AsIs"
+    },
+    "inbounds": [
+        {
+            "listen": "0.0.0.0",
+            "port": ${trojan_port},
+            "protocol": "trojan",
+            "settings": {
+                "clients": [
+                    {
+                        "password": "${trojan_password}"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "tls",
+                "wsSettings": {
+                    "path": "${ws_path}"
+                },
+                "tlsSettings": {
+                    "certificates": [
+                        {
+                            "certificateFile": "${cer_path}",
+                            "keyFile": "${key_path}"
+                        }
+                    ]
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom",
+            "settings": {
+                "domainStrategy": "UseIPv4"
+            }
+        }
+    ]
+}
+EOF
+systemctl restart xray.service
+echo "Trojan 安装成功"
+echo "客户端连接信息: "
+echo "端口: ${trojan_port}"
+echo "密码: ${trojan_password}"
+echo "传输: WebSocket"
+echo "路径: ${ws_path}"
+}
 if [[ $1 == "uninstall" ]]; then
   uninstall
   exit 0
@@ -571,5 +808,17 @@ if [[ $1 == "grpc-tls" ]]; then
 fi
 if [[ $1 == "quic" ]]; then
   quic
+  exit 0
+fi
+if [[ $1 == "vless" ]]; then
+  vless
+  exit 0
+fi
+if [[ $1 == "trojan-tpc" ]]; then
+  trojan-tcp
+  exit 0
+fi
+if [[ $1 == "trojan-ws" ]]; then
+  trojan-ws
   exit 0
 fi
