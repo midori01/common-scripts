148a276128c4af6706f0a20f3d3dcd3c8cc4ef8c
ミドリ <midori01@users.noreply.github.com>
2023-06-26 17:35:29 +0800

    Update install.sh

--- sing-box/install.sh
+++ sing-box/install.sh
@@ -39,7 +39,6 @@ update() {
   echo "sing-box 已更新"
 }
 naive() {
-read -r -p "请输入节点域名: " naive_domain
 read -r -p "请输入证书路径: " cer_path
 read -r -p "请输入私钥路径: " key_path
 read -r -p "请输入节点端口 (留空默认 8964): " naive_port
@@ -54,7 +53,6 @@ if [[ -z "$naive_pass" ]]; then
 fi
 cat <<EOF
 请确认以下配置信息：
-域名：${naive_domain}
 端口：${naive_port}
 用户名：${naive_user}
 密码：${naive_pass}
@@ -129,8 +127,94 @@ echo "客户端连接信息: "
 echo "端口: ${naive_port}"
 echo "用户名: ${naive_user}"
 echo "密码: ${naive_pass}"
-echo "SNI: ${naive_domain}"
 echo "Padding: Enabled"
+}
+hy() {
+read -r -p "请输入证书路径: " cer_path
+read -r -p "请输入私钥路径: " key_path
+read -r -p "请输入节点端口 (留空默认 8964): " hy_port
+hy_port=${hy_port:-8964}
+read -r -p "请输入密码 (留空随机生成): " hy_pass
+if [[ -z "$hy_pass" ]]; then
+  hy_pass=$(openssl rand -hex 8)
+fi
+cat <<EOF
+请确认以下配置信息：
+端口：${hy_port}
+密码：${hy_pass}
+证书路径：${cer_path}
+私钥路径：${key_path}
+EOF
+read -r -p "确认无误？(Y/N)" confirm
+case "$confirm" in
+  [yY]) ;;
+  *) echo "已取消安装"; exit 0;;
+esac
+wget -N --no-check-certificate ${download_url}
+tar zxvf ${package_name}.tar.gz
+mv ${package_name}/sing-box /usr/local/bin/sing-box
+chmod +x /usr/local/bin/sing-box
+rm -r ${package_name}
+rm -f ${package_name}.tar.gz
+cat > /etc/systemd/system/sing-box.service <<EOF
+[Unit]
+After=network.target nss-lookup.target
+
+[Service]
+User=root
+WorkingDirectory=/usr/local/bin
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_RAW
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_RAW
+ExecStart=/usr/local/bin/sing-box run -c /etc/sing-box.json
+ExecReload=/bin/kill -HUP $MAINPID
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=infinity
+
+[Install]
+WantedBy=multi-user.target
+EOF
+cat > /etc/sing-box.json <<EOF
+{
+    "log": {
+        "level": "info",
+        "timestamp": true
+    },
+    "inbounds": [
+        {
+            "type": "naive",
+            "listen": "::",
+            "listen_port": ${hy_port},
+            "up_mbps": 100,
+            "down_mbps": 100,
+            "users": [
+                {
+                    "auth_str": "${hy_pass}"
+                }
+            ],
+            "tls": {
+                "enabled": true,
+                "alpn": [ "h3" ],
+                "certificate_path": "${cer_path}",
+                "key_path": "${key_path}"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "type": "direct"
+        }
+    ]
+}
+EOF
+systemctl daemon-reload
+systemctl start sing-box.service
+systemctl enable sing-box.service
+echo "Hysteria 安装成功"
+echo "客户端连接信息: "
+echo "端口: ${hy_port}"
+echo "密码: ${hy_pass}"
+echo "ALPN: h3"
 }
 if [[ $1 == "uninstall" ]]; then
   uninstall
@@ -142,5 +226,9 @@ if [[ $1 == "update" ]]; then
 fi
 if [[ $1 == "naive" ]]; then
   naive
+  exit 0
+fi
+if [[ $1 == "hy" ]]; then
+  hy
   exit 0
 fi
