39e60a0d6dbeda8adfd74e04e0e720e4cfbacd2e
ミドリ <midori01@users.noreply.github.com>
2023-07-01 23:14:36 +0800

    Update ss-rust.sh

--- shadowsocks/ss-rust.sh
+++ shadowsocks/ss-rust.sh
@@ -2,7 +2,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-sh_ver="2.0.5"
+sh_ver="2.1.0"
 filepath=$(cd "$(dirname "$0")"; pwd)
 file_1=$(echo -e "${filepath}"|awk -F "$0" '{print $1}')
 FILE="/usr/local/bin/ss-rust"
@@ -14,6 +14,46 @@ Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_p
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
 Tip="${Yellow_font_prefix}[注意]${Font_color_suffix}"
+
+simple-obfs() {
+  apt update && apt install --no-install-recommends build-essential autoconf libtool libssl-dev libpcre3-dev libev-dev asciidoc xmlto automake -y
+  git clone https://github.com/shadowsocks/simple-obfs.git
+  cd simple-obfs
+  git submodule update --init --recursive
+  ./autogen.sh
+  ./configure && make
+  make install
+  cd ..
+  rm -r simple-obfs
+  obfs-server -h
+}
+
+v2ray-plugin() {
+  if [[ "$(uname -m)" == "x86_64" ]]; then
+    os_type="amd64"
+  elif [[ "$(uname -m)" == "aarch64" ]]; then
+    os_type="arm64"
+  else
+    exit 1
+  fi
+  latest_version=$(curl -m 10 -sL "https://api.github.com/repos/shadowsocks/v2ray-plugin/releases/latest" | awk -F'"' '/tag_name/{print $4}')
+  wget "https://github.com/shadowsocks/v2ray-plugin/releases/download/${latest_version}/v2ray-plugin-linux-${os_type}-${latest_version}.tar.gz"
+  tar zxvf v2ray-plugin-linux-${os_type}-${latest_version}.tar.gz
+  rm -f v2ray-plugin-linux-${os_type}-${latest_version}.tar.gz
+  mv ./v2ray-plugin_linux_${os_type} /usr/local/bin/v2ray-plugin
+  chmod +x /usr/local/bin/v2ray-plugin
+  v2ray-plugin -version
+}
+
+if [[ $1 == "simple-obfs" ]]; then
+  simple-obfs
+  exit 0
+fi
+
+if [[ $1 == "v2ray-plugin" ]]; then
+  v2ray-plugin
+  exit 0
+fi
 
 check_root(){
 	[[ $EUID != 0 ]] && echo -e "${Error} 当前非ROOT账号(或没有ROOT权限)，无法继续操作，请更换ROOT账号或使用 ${Green_background_prefix}sudo su${Font_color_suffix} 命令获取临时ROOT权限（执行后可能会提示输入当前账号的密码）。" && exit 1
