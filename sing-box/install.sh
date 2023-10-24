d98a48b8d03516bc23d1abccf453ccec64083127
ミドリ <midori01@users.noreply.github.com>
2023-10-24 10:34:58 +0800

    Update install.sh

--- sing-box/install.sh
+++ sing-box/install.sh
@@ -9,7 +9,7 @@ if ! command -v wget &> /dev/null; then
   exit 1
 fi
 if [[ "$(uname -m)" == "x86_64" ]]; then
-  type="amd64v3"
+  type="amd64"
 elif [[ "$(uname -m)" == "aarch64" ]]; then
   type="arm64"
 else
