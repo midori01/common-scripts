1a42ebfafe53329d49d33aaafd8ec3d868b3b7a0
ミドリ <midori01@users.noreply.github.com>
2024-12-12 11:18:10 +0900

    install.sh を更新

--- fping-exporter/install.sh
+++ fping-exporter/install.sh
@@ -20,7 +20,7 @@ install() {
 command -v fping >/dev/null 2>&1 || { if command -v dnf >/dev/null 2>&1 || command -v yum >/dev/null 2>&1; then yum install -y fping || dnf install -y fping; else apt update -qq && apt install -y fping; fi; }
 wget https://github.com/midori01/fping-exporter/releases/download/${latest_version}/fping-exporter-linux-${type}
 mv fping-exporter-linux-${type} /usr/local/bin/fping-exporter
-chmod +x /use/local/bin/fping-exporter
+chmod +x /usr/local/bin/fping-exporter
 cat > /etc/systemd/system/fping-exporter.service <<EOF
 [Unit]
 Description=This is Prometheus Fping-Exporter
